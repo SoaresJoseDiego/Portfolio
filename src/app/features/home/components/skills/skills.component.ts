a67783630305e4c425952e06a5ac2650d5dd3291
SoaresJoseDiego <dsadamovich@gmail.com>
2026-04-02 10:24:19 -0300

    carrosel animado

--- src/app/features/home/components/skills/skills.component.ts
+++ src/app/features/home/components/skills/skills.component.ts
@@ -9,35 +9,11 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-  skillCategories = [
-    {
-      titleKey: 'SKILLS.FRONTEND',
-      icon: 'frontend',
-      skills: [
-        { name: 'Angular', level: 80 },
-        { name: 'TypeScript', level: 75 },
-        { name: 'HTML/CSS', level: 85 },
-        { name: 'SCSS', level: 70 },
-      ],
-    },
-    {
-      titleKey: 'SKILLS.BACKEND',
-      icon: 'backend',
-      skills: [
-        { name: 'C#', level: 75 },
-        { name: '.NET', level: 70 },
-        { name: 'MYSql/SQL Server', level: 65 },
-        { name: 'REST APIs', level: 75 },
-      ],
-    },
-    {
-      titleKey: 'SKILLS.TOOLS',
-      icon: 'tools',
-      skills: [
-        { name: 'Git', level: 80 },
-        { name: 'Azure DevOps', level: 60 },
-        { name: 'Docker', level: 50 },
-      ],
-    },
+  skills = [
+    { name: 'Angular', icon: 'devicon-angular-plain colored' },
+    { name: 'C#', icon: 'devicon-csharp-plain colored' },
+    { name: '.NET', icon: 'devicon-dotnetcore-plain colored' },
+    { name: 'SQL Server', icon: 'devicon-microsoftsqlserver-plain colored' },
+    { name: 'MySQL', icon: 'devicon-mysql-plain colored' },
   ];
 }
